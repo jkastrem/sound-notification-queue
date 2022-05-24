e83d424deb406e77baf46fd41de004428e76602d
jkastrem <drdlemmer@gmail.com>
2022-05-24 18:09:02 +0300

    Replace proxy-object

--- src/notification/index.ts
+++ src/notification/index.ts
@@ -1,26 +1,23 @@
-const soundQueue = (notificationsQueue: Array<HTMLAudioElement>) => {
-  let soundIndex = 0;
+class NotificationsQueue {
+  protected queue: Array<HTMLAudioElement>;
 
-  return new Proxy(notificationsQueue, {
-    set(target, property, value: HTMLAudioElement | number) {
-      if (property === 'length' && typeof value === 'number') {
-        target.length = value;
-      } else if (value instanceof HTMLAudioElement) {
-        target.push(value);
-        value.onended = () => {
-          const nextSound = target[soundIndex += 1];
-          if (nextSound === undefined) {
-            target = [];
-            soundIndex = 0;
-          } else {
-            nextSound.play();
-          }
-        }
-        value.play();
+  constructor() {
+    this.queue = new Proxy<Array<HTMLAudioElement>>([], {
+      set(target, property, value) {
+        if (property === 'length') target.length = value;
+        else if (Number(property) === 0) value.play();
+        else target[Number(property)] = value;
+        return true;
       }
-      return true;
+    })
+  }
+
+  push(sound: HTMLAudioElement) {
+    sound.onended = () => {
+      this.queue.shift()?.play();
     }
-  })
+    this.queue.push(sound);
+  }
 }
 
-export const soundNotifications = soundQueue([])
+export const soundNotifications = new NotificationsQueue();
